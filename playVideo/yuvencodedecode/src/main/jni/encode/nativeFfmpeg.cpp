46628966d61498baba25e4d60c283b5a61883b80
黄金涛 <hjt505694246@163.com>
2020-03-21 16:47:10 +0800

    jni数组未释放内存

--- playVideo/yuvencodedecode/src/main/jni/encode/nativeFfmpeg.cpp
+++ playVideo/yuvencodedecode/src/main/jni/encode/nativeFfmpeg.cpp
@@ -1,5 +1,6 @@
 
 #define  LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"ffmpeg",__VA_ARGS__)
+
 #include "media_jni_MediaNative.h"
 #include "FFmpegEncode.h"
 
@@ -8,6 +9,10 @@ extern "C" {
 #endif
 
 FFmpegEncode *encode = NULL;
+void releaseByteArray(JNIEnv *env, jbyteArray array, uint8_t *elems, jint mode) {
+    env->ReleaseByteArrayElements(array, (jbyte *) elems, mode);
+    env->DeleteLocalRef(array);
+}
 
 JNIEXPORT jint JNICALL Java_media_jni_MediaNative_creatFfmpeg
         (JNIEnv *env, jobject obj) {
@@ -36,17 +41,21 @@ JNIEXPORT jint JNICALL Java_media_jni_MediaNative_initContext
 JNIEXPORT jint JNICALL Java_media_jni_MediaNative_encode
         (JNIEnv *env, jobject obj, jbyteArray src, jbyteArray y, jbyteArray v,
          jbyteArray u, jint w, jint h) {
+    int result = -1;
     uint8_t *srcArray = (uint8_t *) env->GetByteArrayElements(src, 0);
     int lenth = env->GetArrayLength(src);
     uint8_t *yArray = (uint8_t *) env->GetByteArrayElements(y, 0);
     uint8_t *vArray = (uint8_t *) env->GetByteArrayElements(v, 0);
     uint8_t *uArray = (uint8_t *) env->GetByteArrayElements(u, 0);
     if (encode != NULL) {
-        return encode->encodeFFmpeg(srcArray, lenth, yArray, uArray, vArray, &w, &h,
-                                    AV_PIX_FMT_NV21);
-    } else {
-        return -1;
+        result = encode->encodeFFmpeg(srcArray, lenth, yArray, uArray, vArray, &w, &h,
+                                      AV_PIX_FMT_NV21);
     }
+    releaseByteArray(env, src, srcArray, 0);
+    releaseByteArray(env, y, yArray, 0);
+    releaseByteArray(env, v, vArray, 0);
+    releaseByteArray(env, u, uArray, 0);
+    return result;
 } ;
 
 JNIEXPORT void JNICALL Java_media_jni_MediaNative_unFfmpeg
